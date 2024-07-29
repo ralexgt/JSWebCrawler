1f1aa19a1a16da2394f3ad1d3cc71c926506282b
ralexgt <romanalexrares@gmail.com>
2024-07-30 00:08:45 +0300

    init setup | normalize URLs

---
+++ src/crawl.js
@@ -0,0 +1,15 @@
+function normalizeURL(url) {
+	let urlArr = url.split('//');
+	let normalizedURL;
+	if (urlArr.length > 1) {
+		normalizedURL = urlArr[1];
+	} else {
+		normalizedURL = urlArr[0];
+	}
+	if (normalizedURL.slice(-1) === '/') {
+		normalizedURL = normalizedURL.slice(0, -1);
+	}
+	return normalizedURL;
+}
+
+export { normalizeURL };
---
+++ src/tests/crawl.test.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@jest/globals';
+import { normalizeURL } from '../crawl';
+
+test('Input: https://test.com/path1/ | Expected: test.com/path1', () => {
+	expect(normalizeURL('https://test.com/path1/')).toBe('test.com/path1');
+});
+
+test('Input: http://test.com/path2 | Expected: test.com/path2', () => {
+	expect(normalizeURL('http://test.com/path2')).toBe('test.com/path2');
+});
+
+test('Input: test.com/path_s/path3/ | Expected: test.com/path_s/path3', () => {
+	expect(normalizeURL('test.com/path_s/path3/')).toBe('test.com/path_s/path3');
+});
